933634c6ee8c70c2c968b5d811ebeed29f810d6b
Nils Bochow <nils.bochow@uit.no>
2023-02-17 07:54:10 +0100

    Python script for plotting timeseries

--- sic/exclude_no_data.py
+++ sic/exclude_no_data.py
@@ -4,6 +4,7 @@ import h5py
 from matplotlib import pyplot as plt
 #I used 0-100 for ice, 200 for continents and 122 for missing values
 import cartopy.crs as ccrs
+import netCDF4
 
 def exclude_no_data_images(): 
     img_file_path = "/p/tmp/bochow/LAMA/lama/sic/sea_ice_cover_3D_256x256_masked.h5"
@@ -180,8 +181,60 @@ def timeseries():
     plt.savefig(f"/p/tmp/bochow/LAMA/lama/sic/eval_pics/timeseries.png", dpi=600, bbox_inches = "tight")
         
         
-timeseries()
+#timeseries()
 
 
+def timeseries_concentration(): 
+    indices = exclude_no_data_images()[:,0]
+    print(indices.size)
+    sic = np.zeros((indices.size))
+    lat = np.arange(45,90,0.25)
+    lon = np.linspace(-180,180,256)
+    area = np.flip(area_grid(lat, lon), axis = 0)
+    total_area = area.sum(['latitude','longitude'])
+    
+    sic_reanalysis = netCDF4.Dataset("/p/tmp/bochow/LAMA/lama/sic/G10010_SIBT1850_v1.1.nc")["seaice_conc"][:]
+    sic_reanalysis[sic_reanalysis<0] = 0
+    lat_reanalysis = np.flip(netCDF4.Dataset("/p/tmp/bochow/LAMA/lama/sic/G10010_SIBT1850_v1.1.nc")["latitude"][:])
+    lon_reanalysis = netCDF4.Dataset("/p/tmp/bochow/LAMA/lama/sic/G10010_SIBT1850_v1.1.nc")["longitude"][:] -180
+
+    area_reanalysis = np.flip(area_grid(lat_reanalysis, lon_reanalysis), axis = 0)
+    total_area_reanalysis  = area_reanalysis.sum(['latitude','longitude'])
+
+    plt.imshow(area_reanalysis)
+    plt.savefig(f"/p/tmp/bochow/LAMA/lama/sic/eval_pics/test_area.png", dpi=600, bbox_inches = "tight")
+    plt.clf() 
+    plt.imshow(sic_reanalysis[-12,:,:]) 
+    plt.savefig(f"/p/tmp/bochow/LAMA/lama/sic/eval_pics/test_sic_re.png", dpi=600, bbox_inches = "tight")
+    plt.clf() 
+    for k,name in enumerate(indices): 
+        img_gt = Image.open(f'/p/tmp/bochow/LAMA/lama/sic/raw/sic{name:06}.png') 
+        img_inpainted = Image.open(f'/p/tmp/bochow/LAMA/lama/inference/sic/fixed_256/sic{name:06}_crop000_mask000.png')  
+
+        img_gt = unnormalize_pic(np.array(img_gt)[:,:,0])
+        img_inpainted = unnormalize_pic(np.array(img_inpainted)[:,:,0])/100
+        #print(np.sum(img_inpainted))
+        #img_inpainted[img_inpainted<0.15] = 0
+        #print(np.sum(img_inpainted))
+        sic[k] = np.sum((area * img_inpainted)) /1e6
+    
+    sia_reanalysis_mean = np.zeros(sic_reanalysis.shape[0])
+    #sic_reanalysis[sic_reanalysis<15] = 0
+    img_inpainted = Image.open(f'/p/tmp/bochow/LAMA/lama/inference/sic/fixed_256/sic{indices[-1]:06}_crop000_mask000.png')  
+    plt.imshow(img_inpainted) 
+    plt.savefig(f"/p/tmp/bochow/LAMA/lama/sic/eval_pics/test_sic_last.png", dpi=600, bbox_inches = "tight")
+    for i in range(sic_reanalysis.shape[0]):
+        sia_reanalysis_mean[i] = np.sum((area_reanalysis* sic_reanalysis[i,:,:]/100)) /1e6
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    print(sia_reanalysis_mean[-12], sic[-1])
+    ax.scatter(indices+50*12, sic)
+    ax.scatter(np.arange(sia_reanalysis_mean.size), sia_reanalysis_mean)
+    #ax.set_xlim(30*12,112*12)
+    ax.set_xticks(np.arange(30*12, 112*12, 5*12))
+    ax.set_xticklabels(np.arange(1931,2013, 5))
+    plt.savefig(f"/p/tmp/bochow/LAMA/lama/sic/eval_pics/timeseries_comparison.png", dpi=600, bbox_inches = "tight")
+        
+timeseries_concentration()
 
 #plot_sea_ice_inference()
